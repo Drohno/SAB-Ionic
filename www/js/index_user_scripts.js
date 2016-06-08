b82ae7568f9cf1d241f6ad785692f6dd5c5bc840
Drohno <david.roldan.nocelo@gmail.com>
2016-06-08 13:10:15 +0200

    r1
    - Añadido primer header

---
+++ www/js/index_user_scripts.js
@@ -0,0 +1,29 @@
+/*jshint browser:true */
+/*global $ */(function()
+{
+ "use strict";
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+    
+    
+     /* button  Menú */
+    $(document).on("click", ".uib_w_2", function(evt)
+    {
+         /*global uib_sb */
+         /* Other possible functions are: 
+           uib_sb.open_sidebar($sb)
+           uib_sb.close_sidebar($sb)
+           uib_sb.toggle_sidebar($sb)
+            uib_sb.close_all_sidebars()
+          See js/sidebar.js for the full sidebar API */
+        
+         uib_sb.toggle_sidebar($("#slidePrin"));  
+         return false;
+    });
+    
+    }
+ document.addEventListener("app.Ready", register_event_handlers, false);
+})();
